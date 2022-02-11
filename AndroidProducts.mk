7306ae0a48b5577b85928a9ac1332abb7f4e1690
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-02-11 06:58:52 +0100

    TECNO-KF8: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: 1 Manufacturer: tecno Model: TECNO KF8 Script version: 2.1.0 Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_TECNO-KF8.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_TECNO-KF8-user \
+    omni_TECNO-KF8-userdebug \
+    omni_TECNO-KF8-eng
